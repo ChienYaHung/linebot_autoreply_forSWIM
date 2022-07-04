9801f73cd2e965ee076ec8c1f23d1617c7bf5646
CY Hung <y2603578y@gmail.com>
2022-07-04 23:07:06 +0800

    add Wenxian branch template message

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, request, abort
 
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import (
-    MessageEvent,
-    TextMessage,
-    TextSendMessage,
-)
+from linebot.models import (MessageEvent, TextMessage, PostbackEvent,
+                            TextSendMessage, TemplateSendMessage,
+                            ConfirmTemplate, MessageTemplateAction,
+                            ButtonsTemplate, PostbackTemplateAction,
+                            URITemplateAction, CarouselTemplate,
+                            CarouselColumn, ImageCarouselTemplate,
+                            ImageCarouselColumn)
 
 app = Flask(__name__)
 
@@ -37,10 +39,35 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)  #收到訊息時觸發以下函式
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,
-                               TextSendMessage(text=event.message.text))
+    mtext = event.message.text  #接收到的訊息
+    if mtext == "@各店資訊":
+        sendButton(event)
+
+
+def sendButton(event):  #按鈕樣版
+    try:
+        message = TemplateSendMessage(
+            alt_text='按鈕樣板',
+            template=ButtonsTemplate(
+                thumbnail_image_url='https://i.imgur.com/rBpJL3U.jpg',  #顯示的圖片
+                title='北區文賢店',  #主標題
+                text='704台南市北區文賢路795號',  #副標題
+                actions=[
+                    URITemplateAction(  #開啟文賢店google地圖
+                        label='Google地圖',
+                        uri='https://goo.gl/maps/sy7QmR8BaWeaA5qE7'),
+                    MessageTemplateAction(  #文賢店營業時間
+                        label='營業時間', text='@營業時間'),
+                    URITemplateAction(  #文賢店電話，按下後可撥號
+                        label='連絡電話', uri='tel:+886-6-2598198')
+                ]),
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,
+                                   TextSendMessage(text='發生錯誤！'))
 
 
 if __name__ == "__main__":
